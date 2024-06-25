075b9b382a1ccf5442edd371ec7b7c1544f618c7
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-06-25 18:03:20 +0530

    chore: update register component

--- src/components/+register.svelte
+++ src/components/+register.svelte
@@ -3,7 +3,7 @@
 		<div
 			class="font-bold rounded-md bg-[#383838] p-3 px-5 py-3 font-mono text-xs hover:bg-[#484848] text-[#ddd] duration-100"
 		>
-			Register and Get up to $100 free Credit 🎉{' '}
+			Register and Get up to $65 free Credit 🎉{' '}
 		</div>
 	</a>
 </div>
