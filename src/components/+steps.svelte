7a658157e3477cdd931c83aac57bf14381371c86
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-06-25 18:02:02 +0530

    chore: setup

--- src/components/+steps.svelte
+++ src/components/+steps.svelte
@@ -1,48 +1,65 @@
 <script>
-	import Code from './+code.svelte';
-	const zeropsyaml = `project:
-   name: zerops-svelte
+import Code from './+code.svelte';
+const importyaml = `project:
+  name: recipe-solidjs
+  tags:
+    - zerops-recipe
 
-  services:
-   - hostname: sveltenode
+services:
+  - hostname: app
     type: nodejs@20
-    buildFromGit: https://github.com/fxck/zerops-svelte-nodejs
-    ports:
-      - port: 3000
-        httpSupport: true
     enableSubdomainAccess: true
-    minContainers: 1`.trim();
+    buildFromGit: https://github.com/zeropsio/recipe-solidjs-nodejs`.trim();
+
+const zeropsyaml = `zerops:
+  - setup: app
+    build:
+      base: nodejs@20
+      buildCommands:
+        - pnpm i
+        - pnpm run build
+      deployFiles:
+        - build
+        - package.json
+        - node_modules
+    run:
+      base: nodejs@20
+      ports:
+        - port: 3000
+          httpSupport: true
+      start: pnpm start`.trim();
 </script>
 
-<div>
-	<hr class="py-6 border-[#f4f4f4]" />
-	<h2 class="text-center text-4xl font-semibold text-[#333]">Instructions</h2>
-	<div class="grid grid-cols md:grid-cols-2 font-light gap-5 md:gap-10 pt-4">
-		<div class="bg-[#F7F7F7] rounded-md p-8 my-8 flex items-center">
-			<div class="max-w-lg space-y-10">
-				<div>
-					<h3 class="font-semibold text-lg">Step 1</h3>
-					<!-- svelte-ignore a11y-media-has-caption -->
-					<!-- <video width="full" class="rounded-xl mt-3" autoplay loop>
-						<source src="import.mp4" type="video/mp4" />
-					</video> -->
-					<p class="font-medium mt-3">
-						<a href="https://zerops.io" target="_blank"> Create an account </a>{' '}
-						and locate the import button as shown in the video.
-					</p>
-				</div>
-			</div>
-		</div>
-		<div class="flex flex-col">
-			<div class="bg-[#F7F7F7] rounded-md p-8 my-8 flex items-center">
-				<div class="max-w-lg space-y-10">
-					<div>
-						<h3 class="font-semibold text-lg">Step 2</h3>
-						<p class="font-medium">Copy and paste the YAML code and confirm</p>
-					</div>
-				</div>
-			</div>
-			<Code code={zeropsyaml} />
-		</div>
-	</div>
-</div>
+    <div>
+      <div class="flex justify-center">
+        <p class="text-center mx-auto">Deploying will import the following structure (zerops-project-import.yml)<br/>
+        and use following (zerops.yml) instructions to build and deploy your app:</p>
+      </div>
+      <div class="grid grid-cols md:grid-cols-2 font-light gap-5 md:gap-10 pt-4">
+        <div class="flex flex-col gap-5">
+          <Code fileLink="https://github.com/zeropsio/recipe-solid-nodejs/blob/main/zerops-project-import.yml" file="zerops-project-import.yml" code={importyaml} />
+          <div
+          class="flex flex-col py-10 h-[260px] rounded-md gap-5 px-10 bg-[#242424]"
+        >
+          <a
+            href="https://github.com/zeropsio/recipe-svelte-nodejs"
+            target="_blank"
+            class="primarybutton rounded-full text-center text-md duration-300 hover:no-underline"
+            >Recipe Source Code</a>
+          <a
+            href="https://discord.com/invite/WDvCZ54"
+            target="_blank"
+            class="discordbutton rounded-full text-center text-md duration-300 hover:no-underline"
+            >Discord</a>
+          <a
+            href="https://docs.zerops.io"
+            target="_blank"
+            class="zeropsbutton rounded-full text-center text-md duration-300 hover:no-underline"
+            >Documentation</a>
+        </div>
+        </div>
+        <div class="flex flex-col">
+          <Code fileLink="https://github.com/zeropsio/recipe-solid-nodejs/blob/main/zerops.yml" file="zerops.yml" code={zeropsyaml} />
+        </div>
+      </div>
+    </div>
